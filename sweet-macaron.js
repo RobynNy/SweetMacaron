c9b1be5d74042cdcfdffe8aa6503cf17866348b3
RobynNy <robnyss@gmail.com>
2018-08-07 17:39:46 -0700

    first commit

--- sweet-macaron.js
+++ sweet-macaron.js
@@ -1,15 +1,14 @@
 (function() {
 	
-	/* CHANGE THINGS TO CONST IF NOT REASSIGNED or IF NOT i++*/
-	
 	/* Div credit photos on menu page */
-	 let attributionDiv = document.getElementsByClassName('attribution');
-	 let divLength = attributionDiv.length;
+	
+	 const attributionDiv = document.getElementsByClassName('attribution');
+	 const divLength = attributionDiv.length;
      for(i = 0; i < divLength ; i++) {
-	     let divText = document.createTextNode(' By Michelle Naherny ')
+	     const divText = document.createTextNode(' By Michelle Naherny ')
 	     attributionDiv[i].appendChild(divText);
-	     let licenseLink = document.createElement('a');
-	     let licenseText = document.createTextNode('CC BY-SA 4.0');
+	     const licenseLink = document.createElement('a');
+	     const licenseText = document.createTextNode('CC BY-SA 4.0');
 	     licenseLink.appendChild(licenseText);
 	     licenseLink.setAttribute('href', 'https://creativecommons.org/licenses/by-sa/4.0');
 	     attributionDiv[i].appendChild(licenseLink);
@@ -17,10 +16,8 @@
 	 
 	/* Automatic changing slideshow */
 	
-	/* Change window.location.pathname = /index.html when website is uploaded and check it works on home page */
-	
 	let slideIndex = 0;
-	if (window.location.pathname === '/G:/Dreamweaver%20Projects/sweet-macaron-2018/index.html') {
+	if (window.location.pathname === '/G:/portfolio-projects/sweet-macaron-2018/index.html') {
        carousal();
 	}
 	if (window.location.pathname === 'index.html') {
@@ -29,7 +26,7 @@
 	
     function carousal() {
         let i = 0;
-        let x = document.getElementsByClassName('image-slide');
+        const x = document.getElementsByClassName('image-slide');
         for (i = 0; i < x.length; i++) {
             x[i].removeAttribute('id', 'image-slide-display');
         }
@@ -41,7 +38,8 @@
         setTimeout(carousal, 4000); // Image change every 4 seconds
 		
 		/* Below displays first image slide photo credit */
-		let displayCredit = document.getElementsByClassName('credit-div');
+		
+		const displayCredit = document.getElementsByClassName('credit-div');
 		if (slideIndex === 1) {
 			displayCredit[0].setAttribute('id', 'display-credit-home');
 		} else {
@@ -50,11 +48,13 @@
     }
 	
     /* Turns hamburger icon to x */
+	
     function changeIcon() {
         menuButton.classList.toggle("change");
     }
 	
     /* Drop down menu appears after clicking menu icon */
+	
     function dropDown() {
         const menu = document.getElementsByClassName('menu-container')[0];
         if (menu.getAttribute('id') === 'menu-invisible') {
@@ -78,16 +78,18 @@
     }
 
 	/* One-week special offer */
-	let today = new Date();
-	let specialOfferExpire = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
-	let date = specialOfferExpire.getDate();
-	let month = specialOfferExpire.getMonth() + 1;
-	let year = specialOfferExpire.getFullYear();
-	let specialOffer = document.getElementById("special-offer");
+	
+	const today = new Date();
+	const specialOfferExpire = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+	const date = specialOfferExpire.getDate();
+	const month = specialOfferExpire.getMonth() + 1;
+	const year = specialOfferExpire.getFullYear();
+	const specialOffer = document.getElementById("special-offer");
 	specialOffer.textContent += " " + month + "/" + date + "/" + year;
 	
 	/* Changes contact info displayed when links are clicked 
 	   and shows active tab in location tab list */
+	   
 	function locationDisplay(e){
         if (!e) {
 			e = window.event;
@@ -98,12 +100,12 @@
 		} else {
 			e.returnValue = false;
 		}
-		let activeLocationList = document.getElementsByClassName('active-location'); //Location info section
-		let locationTabs = document.getElementsByClassName('location-tabs'); //To grab all location tab links
-		let locationTabClass = locationTabs.className = 'location-tabs'; //Used below to keep the location-tabs class in the html file
+		const activeLocationList = document.getElementsByClassName('active-location'); //Location info section
+		const locationTabs = document.getElementsByClassName('location-tabs'); //To grab all location tab links
+		const locationTabClass = locationTabs.className = 'location-tabs'; //Used below to keep the location-tabs class in the html file
 		
 		for (let i = 0; i < activeLocationList.length; i++) {
-		    if (target.getAttribute('id') === 'location-list') { //prevents function when clicking between location list
+		    if (target.getAttribute('id') === 'location-list') { //Prevents function when clicking between location list
 	            return;
 		    }
 			if (target.getAttribute('class') === 'aside-tab-li') { //Prevents function when clicking li of location tabs
@@ -112,26 +114,27 @@
 			activeLocationList[i].className = 'hidden-location';
 		}
 		
-		for (let i = 0; i < locationTabs.length; i++) {	// changes location tab to be inactive
+		for (let i = 0; i < locationTabs.length; i++) {	//Changes location tab to be inactive
 		    if (locationTabs[i].classList.contains('active-location-tab')) {
 			    locationTabs[i].className = 'non-active-location-tab' + ' ' + locationTabClass;
 			}    
 		}
 		
         /* Below displays the correct info and current tab selected */    
+		
 		if (target.getAttribute('id') === 'irvine'){
 			target.className = 'active-location-tab' + ' ' + locationTabClass;
-			let irvine = document.getElementById('irvine-location');
+			const irvine = document.getElementById('irvine-location');
 			irvine.setAttribute('class', 'active-location');
 		}
 		else if (target.getAttribute('id') === 'la'){
 			target.className = 'active-location-tab'  + ' ' + locationTabClass;
-			let la = document.getElementById('la-location');
+			const la = document.getElementById('la-location');
 			la.setAttribute('class', 'active-location');
 		}
 		else if (target.getAttribute('id') === 'sd') {
 			target.className = 'active-location-tab'  + ' ' + locationTabClass;
-			let sd = document.getElementById('sd-location');
+			const sd = document.getElementById('sd-location');
 			sd.setAttribute('class', 'active-location');
 		} else {
 			return;
@@ -139,7 +142,8 @@
 	}
 	
 	/* Calls function when location tabs are clicked on contact page */
-    let locationButton = document.getElementById('location-list');
+	
+    const locationButton = document.getElementById('location-list');
     if (window.location.pathname === '/G:/Dreamweaver%20Projects/sweet-macaron-2018/sweet-macaron-contact.html' || 'sweet-macaron-contact.html') {
         if (locationButton.addEventListener) {
             locationButton.addEventListener('click', function(e) {
